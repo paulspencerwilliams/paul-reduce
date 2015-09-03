6fa561916abdcb2c2d32aa8cdf70e08ee969f9be
Paul Williams <paul@paulswilliams.me.uk>
2015-09-03 08:09:24 +0100

    Showing stripped down components

--- src/cljs/wat/views.cljs
+++ src/cljs/wat/views.cljs
@@ -1,5 +1,6 @@
 (ns wat.views
-    (:require [re-frame.core :as re-frame]))
+    (:require [re-frame.core :as re-frame]
+              [reagent.core :as reagent]))
 
 (defn input []
   [:div
@@ -7,5 +8,18 @@
    [:input {:type "button"
             :value "add"}]])
 
+(defn graph-render []
+    [:div {:style {:min-width "310px" :max-width "800px"
+                   :height "400px" :margin "0 auto"}}])
+
+(defn graph-did-mount [this])
+
+(defn graph [data]
+  (reagent/create-class {:reagent-render graph-render
+                   :component-did-mount graph-did-mount
+                   :component-did-update graph-did-mount
+                   }))
+
+
 (defn main-panel []
-  [:div [input]])
+  [:div [input][graph]])
