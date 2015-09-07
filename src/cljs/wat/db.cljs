1c5b0dd9599aa64260187c6952bfb2ebe99f8f80
Paul Williams <paul@paulswilliams.me.uk>
2015-09-07 09:20:15 +0100

    fixed y-axis

--- src/cljs/wat/db.cljs
+++ src/cljs/wat/db.cljs
@@ -9,8 +9,9 @@
                     :xAxis       {:type                 :datetime
                                   :dateTimeLabelFormats {:month "%e. %b"
                                                          :year  "%b"}
+
                                   :title                {:text "Date"}}
-                    :yAxis       {:title {:text "Weight (Kg)"} :min 0}
+                    :yAxis       {:title {:text "Weight (Kg)"} :min 0 :max 100}
                     :tooltip     {:headerFormat "<b>{series.name}</b><br>"
                                   :pointFormat  "{point.x:%e. %b}: {point.y:.2f} m"}
                     :plotOptions {:spline {:marker {:enabled true}}}
