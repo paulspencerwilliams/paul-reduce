0428569e1d3c780d403d3d3409b051b2484cb504
Paul Williams <paul@paulswilliams.me.uk>
2015-09-07 08:41:01 +0100

    legends and chart titles

--- src/cljs/wat/db.cljs
+++ src/cljs/wat/db.cljs
@@ -5,13 +5,12 @@
    :entered-weight ""
    :weights        []
    :chart-config   {:chart       {:type :spline}
-                    :title       {:text "Snow depth at Vikjafjellet Norway"}
-                    :subtitle    {:text "Irregular time data in Highcharts JS"}
+                    :title       {:text "(reduce (reduce :pauls-weight))"}
                     :xAxis       {:type                 :datetime
                                   :dateTimeLabelFormats {:month "%e. %b"
                                                          :year  "%b"}
                                   :title                {:text "Date"}}
-                    :yAxis       {:title {:text "Snow depth (m)"} :min 0}
+                    :yAxis       {:title {:text "Weight (Kg)"} :min 0}
                     :tooltip     {:headerFormat "<b>{series.name}</b><br>"
                                   :pointFormat  "{point.x:%e. %b}: {point.y:.2f} m"}
                     :plotOptions {:spline {:marker {:enabled true}}}
