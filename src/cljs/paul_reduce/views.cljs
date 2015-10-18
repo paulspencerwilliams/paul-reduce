da645be6a4b5e2899e3bf263a17dfec29d81c763
Paul Williams <paul@paulswilliams.me.uk>
2015-10-18 18:15:23 +0100

    updated series from 12 stone up

--- src/cljs/paul_reduce/views.cljs
+++ src/cljs/paul_reduce/views.cljs
@@ -57,7 +57,7 @@
                                                                 :year  "%b"}
 
                                          :title                {:text "Date"}}
-                           :yAxis       {:title {:text "Weight (Kg)"} :min 0 :max 100}
+                           :yAxis       {:title {:text "Weight (Kg)"} :min 70 :max 100}
                            :tooltip     {:headerFormat "<b>{series.name}</b><br>"
                                          :pointFormat  "{point.x:%e. %b}: {point.y:.2f} m"}
                            :plotOptions {:spline {:marker {:enabled true}}
